65caba406522811d485df4b44c2ffabb820e3212
I4PE <monicaram@gmail.com>
2022-11-16 22:29:25 -0400

    cambios en atributo de fisico

--- resources/views/resultado.blade.php
+++ resources/views/resultado.blade.php
@@ -22,21 +22,7 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-14xl mx-auto sm:px-12 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                 <h6 class="badge  badge-warning"> COLEGIO MILITAR DEL EJÉRCITO  <br> 
                     “CNL. GUALBERTO VILLARROEL” <br> 
@@ -44,7 +30,7 @@
                 </h6>
                 </div>
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                    <div class="grid grid-cols-1 md:grid-cols-2">
+                    <div class="grid grid-cols-2 md:grid-cols-2">
 
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                             <div class="flex items-center">
@@ -52,18 +38,18 @@
                                 <div class="ml-4 text-lg leading-7 font-semibold"><span class=""> RESULTADO DE SU CONSULTA </span>  </div>
                             </div>
 
-                            <div class="ml-12">
+                            <div class="ml-8">
                                 <div class=" text-sm">
                                     <form name="eval" id="eval"  method="POST" action="evaluacion">
                                     @csrf
                                         <div class="card text-black bg-transparent bg-success mb-3" style="max-width: 30rem;">
                                             @foreach ($data1 as $data1)
                                                 <div class="card-header">
-                                                    <label><b>NOMBRES Y APELLIDOS: </b> </label> 
-                                                    <input type="text" value = "{{ $data1->nombre_completo }}"  id="nombre" name="nombre" readonly> <br>
-                                                    <label><b>NÚMERO DE C.I.:</b></label> 
-                                                    <input type="text" value= {{ $data1->ci }} id="ci" name="ci" readonly>
-                                                    <label><b>NÚMERO DE PROSPECTO:</b></label>
+                                                    <label style="font-size: 8pt;"><b>NOMBRES Y APELLIDOS: </b> </label> 
+                                                    <input type="text" size=37 value = "{{ $data1->nombre_completo }}"  id="nombre" name="nombre" readonly> <br>
+                                                    <label style="font-size: 8pt;"><b>NÚMERO DE C.I.:</b></label> 
+                                                    <input type="text" value= {{ $data1->ci }} id="ci" name="ci" readonly> <br>
+                                                    <label style="font-size: 8pt;"><b>NÚMERO DE PROSPECTO:</b></label>
                                                     <input type="text" value= {{ $data1->codigo }} id="codigo" name="codigo" readonly>
                                                 </div>
                                             @endforeach
@@ -73,7 +59,7 @@
                                                         <tr>
                                                         <th scope="col">Fecha</th>
                                                         <th scope="col">Evaluación</th>
-                                                        <th scope="col">Resultado</th>
+                                                        <th scope="col">Atributo</th>
                                                         </tr>
                                                     </thead>
                                                     <tbody>
@@ -108,7 +94,6 @@
                                 </div>
                             </div>
                         </div>
-
 
                         <div class="p-6">
                             <div class="flex items-center">
